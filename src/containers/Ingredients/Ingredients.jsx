c9caf335e48d362d00080f054a6585e248b3ff33
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-26 18:32:22 -0600

    add ingredient selector to page and change styling to inline-block

--- src/containers/Ingredients/Ingredients.jsx
+++ src/containers/Ingredients/Ingredients.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Sidebar from '../Sidebar/Sidebar'
+import { connect } from 'react-redux'
+// import { getIngredientDetails } from '../../util/api/apiFetch';
+import IngredientCard from '../../components/IngredientCard/IngredientCard';
+import './Ingredients.scss'
+class Ingredients extends Component {
+  constructor(props) {
+    super()
+    this.state={
+      ingredientsList:[]
+    }
+  }
 
-const Ingredients = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  async componentDidMount() {
+    // const ingredientsList = await this.props.userIngredients
+    //   .map(async ingredient => getIngredientDetails(ingredient))
+    // const returned  = await Promise.all(ingredientsList)
+    this.ingredientCards()
+    // this.ingredientCards(returned)
+  }
+
+  ingredientCards = () => {
+    // console.log(cards)
+    return this.props.ingredients.map(item => {
+      return <IngredientCard ingredient={item} />
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Sidebar />
+        <div className="ingredient-cards-container">
+          {this.ingredientCards()}
+        </div>
+      </>
+    )
+  }
 }
+const mapStateToProps = (state) => ({
+  ingredients: state.ingredients
+})
 
-export default Ingredients
+const mapDispatchToProps = (dispatch) => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Ingredients)
