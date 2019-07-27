cc4308bdb6e79bf26c43caef06c6c0b77b9c02d6
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-26 18:25:30 -0600

    add ingredient card functionality and styling

---
+++ src/components/IngredientCard/IngredientCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './IngredientCard.scss'
+const IngredientCard = ({ingredient}) => {
+  return (
+    <div className="ingredient-card">
+      <h3>{ingredient.strIngredient}</h3>
+      <img className="ingredient-image" alt={ingredient.strIngredient} src={`https://www.thecocktaildb.com/images/ingredients/${ingredient.strIngredient}-Medium.png`} /> 
+      <div className="ingredient-description">
+        <p>{ingredient.strDescription || 'No Description supplied'}</p>
+      </div>
+      </div>
+  )
+}
+
+export default IngredientCard
