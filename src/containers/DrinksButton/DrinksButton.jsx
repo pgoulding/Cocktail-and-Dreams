2f29904389dbbeb74752a01379c783aa0e6a9dcc
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-26 18:30:01 -0600

    begin implementation of drink button

--- src/containers/DrinksButton/DrinksButton.jsx
+++ src/containers/DrinksButton/DrinksButton.jsx
@@ -2,19 +2,22 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-export class DrinksButton extends Component {
+export const DrinksButton = ({props}) => {
 
-  render() {
+  const 
+
     return (
       <div>
-        
+        <button onClick>
+
+        </button>
       </div>
     )
-  }
+
 }
 
 const mapStateToProps = (state) => ({
-  
+  ingredients: state.ingredients
 })
 
 const mapDispatchToProps = {
