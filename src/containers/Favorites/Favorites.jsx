d89b59103b269c27bc7ebc385e6f398e27a934b2
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-29 19:18:47 -0600

    WIP favorites section

--- src/containers/Favorites/Favorites.jsx
+++ src/containers/Favorites/Favorites.jsx
@@ -1,24 +1,35 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import DrinkCard from '../../components/DrinkCard/DrinkCard';
 
 export class Favorites extends Component {
   constructor(props) {
-    super()
-  }
-  showFavorites = () => {
-    return this.props.favoriteCocktails.map(drink => {
-      return <p>{ drink }</p>
-    })
+    super(props)
+    this.state = {
+      savedCocktails:[]
+    }
   }
 
+  componentDidMount() {
+    const { favoriteCocktails } = this.props
+    console.log(this.props)
+  }
+
+  showFavorites = () => {
+    return this.state.savedCocktails.map(drink => {
+      return <DrinkCard drinkInfo={drink} />
+    })
+  }
+  
+  
   render() {
-    if(!this.props.favoriteCocktails) {
+    if(!this.state.favoriteCocktails) {
       return (
         <h2>No Favorite Cocktails added yet</h2>
-      )
-    }
-    return (
+        )
+      }
+      return (
       <div>
         {this.showFavorites()}
       </div>
