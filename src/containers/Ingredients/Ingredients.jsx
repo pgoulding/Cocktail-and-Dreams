1066329cf0f22a4207e35c2206cb957b780ec163
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-29 19:19:20 -0600

    change method to send ingredient details to store

--- src/containers/Ingredients/Ingredients.jsx
+++ src/containers/Ingredients/Ingredients.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import Sidebar from '../Sidebar/Sidebar'
 import { connect } from 'react-redux'
-// import { getIngredientDetails } from '../../util/api/apiFetch';
 import IngredientCard from '../../components/IngredientCard/IngredientCard';
 import './Ingredients.scss'
 class Ingredients extends Component {
@@ -12,16 +11,7 @@ class Ingredients extends Component {
     }
   }
 
-  async componentDidMount() {
-    // const ingredientsList = await this.props.userIngredients
-    //   .map(async ingredient => getIngredientDetails(ingredient))
-    // const returned  = await Promise.all(ingredientsList)
-    this.ingredientCards()
-    // this.ingredientCards(returned)
-  }
-
   ingredientCards = () => {
-    // console.log(cards)
     return this.props.ingredients.map(item => {
       return <IngredientCard ingredient={item} />
     })
