41d283028fbfaaf85b1c5c5298ad2240a79834af
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-30 17:52:22 -0600

    redesign favorites container to show favorite drinks

--- src/containers/Favorites/Favorites.jsx
+++ src/containers/Favorites/Favorites.jsx
@@ -2,39 +2,24 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import DrinkCard from '../../components/DrinkCard/DrinkCard';
+import './Favorites.scss'
+const Favorites =({favoriteCocktails}) => {
 
-class Favorites extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      savedCocktails:[]
+  const showFavorites = () => {
+    if(favoriteCocktails.length) {
+      return favoriteCocktails.map(drink => {
+        return <DrinkCard drinkInfo={drink} />
+      })
+    } else {
+      return <h2>No Favorites added Yet...</h2>
     }
   }
 
-  componentDidMount() {
-    const { favoriteCocktails } = this.props
-    console.log(this.props)
-  }
-
-  showFavorites = () => {
-    return this.state.savedCocktails.map(drink => {
-      return <DrinkCard drinkInfo={drink} />
-    })
-  }
-  
-  
-  render() {
-    if(!this.state.favoriteCocktails) {
-      return (
-        <h2>No Favorite Cocktails added yet</h2>
-        )
-      }
-      return (
-      <div>
-        {this.showFavorites()}
-      </div>
-    )
-  }
+  return (
+    <div className="favorites-container">
+      {showFavorites()}
+    </div>
+  )
 }
 
 const mapStateToProps = ({favoriteCocktails}) => ({
